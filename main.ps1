885f5a414ebe85f8836c287daf03effe772f96c3
BinBaldin <89844198+BinBaldin@users.noreply.github.com>
2024-05-21 21:46:39 +0200

    first

---
+++ main.ps1
@@ -0,0 +1,39 @@
+# Funkce pro vytvoření souboru nebo složky
+function FunctionName {
+    param (
+        OptionalParameters
+    )
+    
+}
+
+
+# Funkce pro přesun
+function FunctionName {
+    param (
+        OptionalParameters
+    )
+    
+}
+
+
+
+# Fuknce pro smazaní složky nebo adresáře
+function FunctionName {
+    param (
+        OptionalParameters
+    )
+    
+}
+
+
+# Funkce pro zálohu
+function FunctionName {
+    param (
+        OptionalParameters
+    )
+    
+}
+
+
+
+
